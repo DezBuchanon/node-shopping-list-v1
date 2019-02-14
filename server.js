023caf5aa87c2b7587210dec6718dc8589ba39fa
Zsadez Buchanon <zdbuchanon@gmail.com>
2019-02-13 21:11:34 -0500

    update

--- server.js
+++ server.js
@@ -16,17 +16,6 @@ const app = express();
 // log the http layer
 app.use(morgan('common'));
 
-
-
-
-
-////////////////// TEST
-
-
-
-
-
-
 // we're going to add some items to ShoppingList
 // so there's some data to look at. Note that 
 // normally you wouldn't do this. Usually your
